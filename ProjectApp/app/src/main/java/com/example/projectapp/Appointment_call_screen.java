d5673c4c30d4394afa81b13380d08f70addbb93a
Quan Nguyen <walker30122003@gmail.com>
2021-10-27 19:36:11 +0700

    Add book_confirmation_screen

---
+++ ProjectApp/app/src/main/java/com/example/projectapp/Appointment_call_screen.java
@@ -0,0 +1,14 @@
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Appointment_call_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_call_screen);
+    }
+}
---
+++ ProjectApp/app/src/main/java/com/example/projectapp/BookConfirmation_screen.java
@@ -0,0 +1,14 @@
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookConfirmation_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_confirmation_screen);
+    }
+}
