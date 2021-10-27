0bd0708a84a942cf05e63d62f9c3a796a8b37a93
Tui tên Quang <quanghn19411@st.uel.edu.vn>
2021-10-27 12:04:35 +0700

    Update icon and confirm screen

---
+++ ProjectApp/app/src/main/java/com/example/projectapp/confirm_screen.java
@@ -0,0 +1,18 @@
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class confirm_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_screen);
+
+    }
+}
