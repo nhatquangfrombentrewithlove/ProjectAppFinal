9e1c6f850a0b411798bc48d4460bb18f63a0c237
MacroNg07 <datnp07@gmail.com>
2021-10-30 23:42:01 +0700

    note + user choose

---
+++ ProjectApp/app/src/main/java/com/example/projectapp/DoctorList.java
@@ -0,0 +1,16 @@
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorList extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_list);
+    }
+}
