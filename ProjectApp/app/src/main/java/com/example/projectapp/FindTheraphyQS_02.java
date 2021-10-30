e889f9e1f3679de3c20793ccef481ec256ac58ac
PhanGiang135 <92997632+PhanGiang135@users.noreply.github.com>
2021-10-30 16:37:07 +0700

    Find Theraphy 1

---
+++ ProjectApp/app/src/main/java/com/example/projectapp/FindTheraphyQS_02.java
@@ -0,0 +1,14 @@
+package com.example.projectapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindTheraphyQS_02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_theraphy_qs02);
+    }
+}
